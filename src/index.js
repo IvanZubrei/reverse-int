73e88d0309d7c1e1c5a25d6ecd972b140a638c92
Ivan Zubrei <zubreiivan@gmail.com>
2024-07-30 09:03:07 +0300

    init: add function to reverse a string in a loop for

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverseString(n) {
+  if (n < 0) {
+  let reversed = '';
+  const m = String(n * -1);  
+  for (let i = m.length - 1; i >= 0; i--) {
+    reversed = reversed + m[i];     
+  }
+  console.log(reversed);
+  return reversed;
+} else {
+  let reversed = '';
+  const m = String(n);  
+  for (let i = m.length - 1; i >= 0; i--) {
+    reversed = reversed + m[i];     
+  }
+  console.log(reversed);
+  return reversed;
 }
+}
+
